43c034ac30fbcacfef23def27f30357b49779adf
SarveshtheAbstractor <hiwasesarvesh@yahoo.com>
2020-12-26 13:54:47 +0530

    adding api bug fix

--- src/utils/quizgen.js
+++ src/utils/quizgen.js
@@ -1,73 +1,106 @@
 const axios = require("axios");
 
-let token = null;
-
-const apiUrl = `https://opentdb.com/api_token.php?command=request`;
-
-const questionUrl = (token, limit, categoryValue) =>
-  `https://opentdb.com/api.php?amount=${limit}&difficulty=easy&type=multiple${categoryValue}&token=${token}`;
-
 const getQuestions = async (limit, categoryValue) => {
   if (categoryValue !== "any") {
     categoryValue = `&category=${categoryValue}`;
   } else {
     categoryValue = "";
   }
-  let questions = [];
-  if (!token) {
-    await getToken();
-  }
-  const { response_code: rc, results: resultParent } = await jsonData(
-    limit,
-    categoryValue
-  );
 
-  if (rc === 4) {
-    await getToken();
-    const { results } = await jsonData(limit, categoryValue);
-    results.forEach((el) => {
-      const answers = {
-        a: el.incorrect_answers[0],
-        b: el.incorrect_answers[1],
-        c: el.incorrect_answers[2],
-        d: el.correct_answer,
-      };
+  let quizQuestions = [];
+  const apiUrl = `https://opentdb.com/api.php?amount=${limit}&difficulty=easy&type=multiple${categoryValue}`;
 
-      questions.push({
-        question: el.question,
-        ...answers,
+  const jsondata = await axios(apiUrl);
+  const array = jsondata.data.results;
 
-        correct: el.correct_answer,
-      });
+  array.forEach((el) => {
+    const answers = {
+      a: el.incorrect_answers[0],
+      b: el.incorrect_answers[1],
+      c: el.incorrect_answers[2],
+      d: el.correct_answer,
+    };
+
+    quizQuestions.push({
+      question: el.question,
+      ...answers,
+
+      correct: el.correct_answer,
     });
-  } else {
-    resultParent.forEach((el) => {
-      const answers = {
-        a: el.incorrect_answers[0],
-        b: el.incorrect_answers[1],
-        c: el.incorrect_answers[2],
-        d: el.correct_answer,
-      };
+  });
 
-      questions.push({
-        question: el.question,
-        ...answers,
-
-        correct: el.correct_answer,
-      });
-    });
-  }
-
-  return questions;
+  return quizQuestions;
 };
 
-async function jsonData(limit, categoryValue) {
-  const result = await axios(questionUrl(token, limit, categoryValue));
-  return result.data;
-}
-
-const getToken = async () => {
-  const result = await axios(apiUrl);
-  token = result.data.token;
-};
 module.exports = getQuestions;
+
+// let token = null;
+
+// const apiUrl = `https://opentdb.com/api_token.php?command=request`;
+
+// const questionUrl = (token, limit, categoryValue) =>
+//   `https://opentdb.com/api.php?amount=${limit}&difficulty=easy&type=multiple${categoryValue}&token=${token}`;
+
+// const getQuestions = async (limit, categoryValue) => {
+//   if (categoryValue !== "any") {
+//     categoryValue = `&category=${categoryValue}`;
+//   } else {
+//     categoryValue = "";
+//   }
+//   let questions = [];
+//   if (!token) {
+//     await getToken();
+//   }
+//   const { response_code: rc, results: resultParent } = await jsonData(
+//     limit,
+//     categoryValue
+//   );
+
+//   if (rc === 4) {
+//     await getToken();
+//     const { results } = await jsonData(limit, categoryValue);
+//     results.forEach((el) => {
+//       const answers = {
+//         a: el.incorrect_answers[0],
+//         b: el.incorrect_answers[1],
+//         c: el.incorrect_answers[2],
+//         d: el.correct_answer,
+//       };
+
+//       questions.push({
+//         question: el.question,
+//         ...answers,
+
+//         correct: el.correct_answer,
+//       });
+//     });
+//   } else {
+//     resultParent.forEach((el) => {
+//       const answers = {
+//         a: el.incorrect_answers[0],
+//         b: el.incorrect_answers[1],
+//         c: el.incorrect_answers[2],
+//         d: el.correct_answer,
+//       };
+
+//       questions.push({
+//         question: el.question,
+//         ...answers,
+
+//         correct: el.correct_answer,
+//       });
+//     });
+//   }
+
+//   return questions;
+// };
+
+// async function jsonData(limit, categoryValue) {
+//   const result = await axios(questionUrl(token, limit, categoryValue));
+//   return result.data;
+// }
+
+// const getToken = async () => {
+//   const result = await axios(apiUrl);
+//   token = result.data.token;
+// };
